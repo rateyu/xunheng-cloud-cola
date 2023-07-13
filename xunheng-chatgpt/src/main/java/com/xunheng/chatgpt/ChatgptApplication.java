f446134620510713de5c964458b11ea017ff710b
hhqkkr <452794162@qq.com>
2023-07-13 23:01:34 +0800

    启动类添加忽略java11的非法反射警告

--- xunheng-chatgpt/src/main/java/com/xunheng/chatgpt/ChatgptApplication.java
+++ xunheng-chatgpt/src/main/java/com/xunheng/chatgpt/ChatgptApplication.java
@@ -12,6 +12,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -26,6 +28,7 @@ public class ChatgptApplication {
     private String apiHost;
 
     public static void main(String[] args) {
+        disableAccessWarnings();
         SpringApplication.run(ChatgptApplication.class, args);
     }
 
@@ -52,5 +55,26 @@ public class ChatgptApplication {
                 .keyStrategy(new KeyRandomStrategy())
                 .okHttpClient(okHttpClient)
                 .build();
+    }
+
+    /**
+     * 忽略非法反射警告 适用于jdk11
+     */
+    @SuppressWarnings("unchecked")
+    public static void disableAccessWarnings() {
+        try {
+            Class unsafeClass = Class.forName("sun.misc.Unsafe");
+            Field field = unsafeClass.getDeclaredField("theUnsafe");
+            field.setAccessible(true);
+            Object unsafe = field.get(null);
+            Method putObjectVolatile =
+                    unsafeClass.getDeclaredMethod("putObjectVolatile", Object.class, long.class, Object.class);
+            Method staticFieldOffset = unsafeClass.getDeclaredMethod("staticFieldOffset", Field.class);
+            Class loggerClass = Class.forName("jdk.internal.module.IllegalAccessLogger");
+            Field loggerField = loggerClass.getDeclaredField("logger");
+            Long offset = (Long)staticFieldOffset.invoke(unsafe, loggerField);
+            putObjectVolatile.invoke(unsafe, loggerClass, offset, null);
+        } catch (Exception ignored) {
+        }
     }
 }
--- xunheng-log/xunheng-log-start/src/main/java/com/xunheng/log/LogApplication.java
+++ xunheng-log/xunheng-log-start/src/main/java/com/xunheng/log/LogApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 @SpringBootApplication(scanBasePackages = {"com.xunheng", "com.alibaba.cola"})
 @EnableXhFeignClients
 @XhMvc
@@ -13,6 +16,28 @@ import org.springframework.data.elasticsearch.repository.config.EnableElasticsea
 public class LogApplication {
 
     public static void main(String[] args) {
+        disableAccessWarnings();
         SpringApplication.run(LogApplication.class, args);
+    }
+
+    /**
+     * 忽略非法反射警告 适用于jdk11
+     */
+    @SuppressWarnings("unchecked")
+    public static void disableAccessWarnings() {
+        try {
+            Class unsafeClass = Class.forName("sun.misc.Unsafe");
+            Field field = unsafeClass.getDeclaredField("theUnsafe");
+            field.setAccessible(true);
+            Object unsafe = field.get(null);
+            Method putObjectVolatile =
+                    unsafeClass.getDeclaredMethod("putObjectVolatile", Object.class, long.class, Object.class);
+            Method staticFieldOffset = unsafeClass.getDeclaredMethod("staticFieldOffset", Field.class);
+            Class loggerClass = Class.forName("jdk.internal.module.IllegalAccessLogger");
+            Field loggerField = loggerClass.getDeclaredField("logger");
+            Long offset = (Long)staticFieldOffset.invoke(unsafe, loggerField);
+            putObjectVolatile.invoke(unsafe, loggerClass, offset, null);
+        } catch (Exception ignored) {
+        }
     }
 }
